6c8a87bca68cd70decc4c17bd154f9917d3691b8
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-07-30 14:42:50 -0300

    Initial Commit

---
+++ EventHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EventHub
+//
+//  Created by Rodrigo Cerqueira Reis on 30/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EventHub/EventHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventHubApp.swift
+//  EventHub
+//
+//  Created by Rodrigo Cerqueira Reis on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct EventHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
